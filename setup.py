75742f482f98926374df04645c0e177e76688c66
José Luis Di Biase <josx@interorganic.com.ar>
2021-03-05 09:29:44 -0300

    New Release v0.0.8
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="oodocument",
-    version="0.0.7",
+    version="0.0.8",
     author="José Luis Di Biase",
     author_email="josx@camba.coop",
     description="Basic tasks to convert and replace files using uno api",
